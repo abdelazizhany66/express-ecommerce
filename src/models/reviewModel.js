3799719f896a2f2b8796549a590c3198236e3f96
abdelazizhany66 <adhmh080@gmail.com>
2025-10-15 08:00:01 +0200

    feat: add aggrigation pipeline

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -25,7 +25,40 @@ const reviewSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+reviewSchema.statics.calcAvarageRatingAndQuantity = async function (productId) {
+  const result = await this.aggregate([
+    {
+      $match: { product: productId },
+    },
+    {
+      $group: {
+        _id: productId,
+        ratingAvarage: { $avg: '$ratings' },
+        ratingQuantity: { $sum: 1 },
+      },
+    },
+  ]);
+  if (result.length > 0) {
+    await product.findByIdAndUpdate(productId, {
+      ratingAvarage: result[0].ratingAvarage,
+      ratingQuantity: result[0].ratingQuantity,
+    });
+  } else {
+    await product.findByIdAndUpdate(productId, {
+      ratingAvarage: 0,
+      ratingQuantity: 0,
+    });
+  }
+};
 
+reviewSchema.post('save', async function () {
+  // this points to current review
+  await this.constructor.calcAvarageRatingAndQuantity(this.product);
+});
+
+reviewSchema.post('deleteOne', { document: true }, async function () {
+  await this.constructor.calcAvarageRatingAndQuantity(this.product);
+});
 
 reviewSchema.pre(/^find/, function (next) {
   this.populate({ path: 'user', select: 'name' });
